7683e985f638c1ea0e02e4ea48608ff3d6f0190c
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-03-02 10:23:27 -0600

    Create MainActivity.java

---
+++ MortgageV1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mortgagecalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+// this version does not have functionality!!!
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void modifyData(View view) {
+
+
+    }
+}
