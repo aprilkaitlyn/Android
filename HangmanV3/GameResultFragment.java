5bb84f4c56333bffa090ec0f5d9a665abcb0d591
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-03-12 12:39:41 -0500

    Create GameResultFragment.java

---
+++ HangmanV3/GameResultFragment.java
@@ -0,0 +1,39 @@
+package com.example.hangmanv3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GameResultFragment extends Fragment { //this fragment does not have an XML
+
+    TextView TXT_gameResult;
+
+    public GameResultFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setUpFragmentGUI(container);
+        return super.onCreateView(inflater, container, savedInstanceState); //typing onCreateView brought up the following 3
+    }
+
+    private void setUpFragmentGUI(ViewGroup container) {
+        if(TXT_gameResult == null) { //if it doesn't exist we want a new one
+            TXT_gameResult= new TextView(getActivity());
+            TXT_gameResult.setGravity(Gravity.CENTER);
+            container.addView(TXT_gameResult);
+        }
+    }
+
+    public void onStart() {
+        super.onStart();
+        TXT_gameResult.setText("Good Luck!");
+    }
+}
