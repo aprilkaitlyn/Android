99ab478990afabc699fc95f449bc24816b63582f
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-03-12 12:08:02 -0500

    Create MainActivity.java

---
+++ HangmanV0/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hangmanv0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //new --> fragment --> blank --> makes a java & XML --> stunning!! wow!!
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
