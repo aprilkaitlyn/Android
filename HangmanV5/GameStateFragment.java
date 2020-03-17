6d06bd44945d4f2a33979ebe8aaaebbcb4b6acb9
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-03-17 11:39:27 -0500

    Create GameStateFragment.java

---
+++ HangmanV5/GameStateFragment.java
@@ -0,0 +1,33 @@
+package com.example.hangmanv5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+
+public class GameStateFragment extends Fragment {
+
+    public GameStateFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game_state, container, false);
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        View fragmentView = getView(); //"getting primary view"
+        TextView gameStateTV = fragmentView.findViewById(R.id.state_of_game);
+        MainActivity fragmentActivity = (MainActivity) getActivity();
+        gameStateTV.setText(fragmentActivity.getGame().currentIncompleteWord());
+    }
+
+}
