4fff2194fc80214f6ee1e1ed3dfa417b9a6d220b
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-03-17 11:38:49 -0500

    Create BackgroundFragment.java

---
+++ HangmanV5/BackgroundFragment.java
@@ -0,0 +1,18 @@
+package com.example.hangmanv5;
+
+
+import androidx.fragment.app.Fragment;
+
+
+public class BackgroundFragment extends Fragment {
+
+
+    public BackgroundFragment() {
+        // Required empty public constructor
+    }
+
+    public String warning() {
+        return "ONLY 1 LEFT!";
+    }
+
+}
