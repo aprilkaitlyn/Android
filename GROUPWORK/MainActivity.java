0d7e44b3bac0aec9f6fc4f48e25fd7654bcdde1a
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-05-06 00:27:22 -0500

    Update MainActivity.java

--- GROUPWORK/MainActivity.java
+++ GROUPWORK/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     TaxiMath taxi;
-
+    DatabaseManager dbManager;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             taxi = new TaxiMath();
+            dbManager = new DatabaseManager(this);
+
 
         //FragmentManager gives the tools for working with fragments
         FragmentManager fm = getSupportFragmentManager();
@@ -31,5 +40,30 @@ public class MainActivity extends AppCompatActivity {
             ft.add(R.id.FRAG_database, fragment);
             ft.commit(); //need this for fragment to be visible
         }
+    }
+
+    private void createView() { //same as update activity but no ability to edit
+        ArrayList<Taxi> taxis = dbManager.selectAll();
+        RelativeLayout layout = new RelativeLayout(this);
+        ScrollView scrollView = new ScrollView(this);
+        RadioGroup group = new RadioGroup(this);
+        for (Taxi taxi : taxis) {
+            TextView friendList = new TextView(this);
+            taxiList.setId(taxi.getId());
+            taxiList.setText(taxi.get() + taxi.get() + taxi.get()); //concatenate name + base price + price per mile
+            taxiList.setTextSize(20);
+            group.addView(taxiList);
+        }
+
+        scrollView.addView(group);
+        layout.addView(scrollView);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.setMargins(0, 0, 0, 50);
+        layout.setPadding(50,50,50,50);
+
+        setContentView(layout);
     }
 }
